aa677b6d65c4d51579387d358db39114c607e7ed
IgorKashintsev <igorchello99@yandex.ru>
2022-05-24 18:35:28 +0300

    Lesson 1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,48 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld :msg="msg" />
+    <p>{{ msg }}</p>
+    <!-- <button @click="counter++">{{ counter }}</button> -->
+    <!-- <button @click="inc(2, $event)">{{ counter }}</button>
+    <button @click="reset">Reset counter</button> -->
+    <input type="text" placeholder="text" v-model="someText" @focus="focus" @blur="blur">
+    <p>{{ someText }}</p>
+    <CalcComponent></CalcComponent>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import CalcComponent from './components/CalcComponent.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      msg: 'Hello Vue!',
+      counter: 0,
+      someText: '',
+    }
+  },
+  methods: {
+    inc(step, event) {
+      console.log(event);
+      this.counter++;
+    },
+    reset() {
+      this.counter = 0;
+    },
+    focus() {
+      console.log('focus');
+    },
+    blur() {
+      console.log('blur');
+    }
+  },
   components: {
-    HelloWorld
+    HelloWorld,
+    CalcComponent
   }
 }
 </script>
---
+++ src/components/CalcComponent.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="main">
+      <input type="number" v-model.number="op1">
+      <input type="number" v-model.number="op2">
+      = {{result}}
+    </div>
+    <div>
+      <button @click="sum">+</button>
+      <button @click="sub">-</button>
+      <button @click="div">/</button>
+      <button @click="mult">*</button>
+      <button @click="expon">*ex</button>
+      <button @click="divInteger">/in</button> 
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CalcComponent',
+  data() {
+    return {
+      op1: 0,
+      op2: 0,
+      result: 0,
+    }
+  },
+  methods: {
+    sum() {
+      this.result = this.op1 + this.op2;
+    },
+    sub() {
+      this.result = this.op1 - this.op2;
+    },
+    div() {
+      this.result = this.op1 / this.op2;
+    },
+    mult() {
+      this.result = this.op1 * this.op2;
+    },
+    expon() {
+      this.result = Math.pow(this.op1, this.op2);
+    },
+    divInteger() {
+      this.result = (this.op1 - this.op1 % this.op2)/this.op2;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
